00ef78661c9ae7f5d89cc770ded8d9f62c99ce4d
filippob <filippo.biscarini@gmail.com>
2021-12-13 16:54:52 +0100

    updating primer sequences for 16S sequencinh

--- scripts_Bacteria/3.remove_adapters.sh
+++ scripts_Bacteria/3.remove_adapters.sh
@@ -2,6 +2,9 @@
 currpath=$(pwd)
 datapath="/home/ngs/210915_M04028_0141_000000000-JY8M4/"
 core=8
+## primer sequences (primers follow the adapters)
+fwd_primer="CCTACGGGNGGCWGCAG" #(adapter: TCGTCGGCAGCGTCAGATGTGTATAAGAGACAG)
+rev_primer="GACTACHVGGGTATCTAATCC" #(adapter: GTCTCGTGGGCTCGGAGATGTGTATAAGAGACAG)
 
 export PATH=/home/biscarinif/.local/bin:$PATH
 
@@ -47,7 +50,7 @@ cd $currpath/renamed
 while read file
 do
 	echo "Running cutadapt on file "${file}""
-	/usr/local/bin/cutadapt -g Forward=CCTACGGGNGGCWGCAG -G Reverse=GACTACNVGGGTWTCTAATCC --discard-untrimmed --pair-filter=any -o $currpath/cutadapt/"${file}_R1_cutadapt.fastq.gz" -p $currpath/cutadapt/"${file}_R2_cutadapt.fastq.gz" "${file}_R1.fastq.gz" "${file}_R2.fastq.gz" >> $currpath/quality_control/cutadapt/cutadapt_report.txt  
+	/usr/local/bin/cutadapt -g Forward=${fwd_primer} -G Reverse=${rev_primer} --discard-untrimmed --pair-filter=any -o $currpath/cutadapt/"${file}_R1_cutadapt.fastq.gz" -p $currpath/cutadapt/"${file}_R2_cutadapt.fastq.gz" "${file}_R1.fastq.gz" "${file}_R2.fastq.gz" >> $currpath/quality_control/cutadapt/cutadapt_report.txt  
 done < names_single.txt
 
 # --discard-untrimmed, --trimmed-only
