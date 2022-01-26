e01c50553f382f67b2c950c5e12d98ce73d3692d
filippob <filippo.biscarini@gmail.com>
2022-01-26 19:00:33 +0100

    fixing paths

--- scripts_Bacteria/3.remove_adapters.sh
+++ scripts_Bacteria/3.remove_adapters.sh
@@ -1,6 +1,7 @@
 ## setting the enviornmnent
 currpath=$(pwd)
-datapath="/home/ngs/210915_M04028_0141_000000000-JY8M4/"
+datapath="/home/ngs/200206_M04028_0114_000000000-CWB58/"
+outdir="Analysis/bacteria"
 core=8
 ## primer sequences (primers follow the adapters)
 fwd_primer="CCTACGGGNGGCWGCAG" #(adapter: TCGTCGGCAGCGTCAGATGTGTATAAGAGACAG)
@@ -9,26 +10,26 @@ rev_primer="GACTACHVGGGTATCTAATCC" #(adapter: GTCTCGTGGGCTCGGAGATGTGTATAAGAGACAG
 export PATH=/home/biscarinif/.local/bin:$PATH
 
 ## make folders
-if [ ! -d "Analysis/cutadapt" ]; then
-	mkdir Analysis/cutadapt
+if [ ! -d "${outdir}/cutadapt" ]; then
+	mkdir -p ${outdir}/cutadapt
 fi
 
-if [ ! -d "Analysis/trimmed" ]; then
-	mkdir Analysis/trimmed
+if [ ! -d "${outdir}/trimmed" ]; then
+	mkdir -p ${outdir}/trimmed
 fi
 
-if [ ! -d "Analysis/micca_its" ]; then
-	mkdir Analysis/micca_its
+if [ ! -d "${outdir}/micca_its" ]; then
+	mkdir -p ${outdir}/micca_its
 fi
 
-if [ ! -d "Analysis/quality_control" ]; then
-	mkdir Analysis/quality_control
+if [ ! -d "${outdir}/quality_control" ]; then
+	mkdir -p ${outdir}/quality_control
 fi
 
 
 ## Create a file of names that will be used for looping. Only file/sample name, remove extension and R1/R2
 
-cd $currpath/Analysis/renamed
+cd "${currpath}/${outdir}/renamed"
 
 for i in *.fastq.gz
 do
@@ -36,21 +37,23 @@ echo "$i" | cut -d "_" -f1 >> names.txt
 sed 'n; d' names.txt > names_single.txt
 done
 
-cp names_single.txt $currpath/Analysis/trimmed
-cp names_single.txt $currpath/Analysis/cutadapt
-cp names_single.txt $currpath/Analysis/micca_16S
+cp names_single.txt ${currpath}/${outdir}/trimmed
+cp names_single.txt ${currpath}/${outdir}/cutadapt
+cp names_single.txt ${currpath}/${outdir}/micca_16S
 
 # remove primers with cutadapt
 # Primers (Sequences from Pindo and FMACH)
 # forward: CCTACGGGNGGCWGCAG
 # reverse: GACTACNVGGGTWTCTAATCC
 
-cd $currpath/renamed
+cd "${currpath}/${outdir}/renamed"
+echo $(pwd)
 
 while read file
 do
-	echo "Running cutadapt on file "${file}""
-	/usr/local/bin/cutadapt -g Forward=${fwd_primer} -G Reverse=${rev_primer} --discard-untrimmed --pair-filter=any -o $currpath/cutadapt/"${file}_R1_cutadapt.fastq.gz" -p $currpath/cutadapt/"${file}_R2_cutadapt.fastq.gz" "${file}_R1.fastq.gz" "${file}_R2.fastq.gz" >> $currpath/quality_control/cutadapt/cutadapt_report.txt  
+	echo "Running cutadapt on file '${file}'"
+	cutadapt -g Forward=${fwd_primer} -G Reverse=${rev_primer} --discard-untrimmed --pair-filter=any -o "${currpath}/${outdir}/cutadapt/${file}_R1_cutadapt.fastq.gz" -p "${currpath}/${outdir}/cutadapt/${file}_R2_cutadapt.fastq.gz" "${file}_R1.fastq.gz" "${file}_R2.fastq.gz" >> "${currpath}/${outdir}/quality_control/cutadapt_report.txt"  
+
 done < names_single.txt
 
 # --discard-untrimmed, --trimmed-only
