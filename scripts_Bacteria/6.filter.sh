92e0e65e380555d3a234b28684d0e3616430d8d0
filippob <filippo.biscarini@gmail.com>
2022-01-27 10:54:09 +0100

    adding variables and comments

--- scripts_Bacteria/6.filter.sh
+++ scripts_Bacteria/6.filter.sh
@@ -1,17 +1,20 @@
 ## setting the enviornmnent
 currpath=$(pwd)
-datapath="/home/ngs/210915_M04028_0141_000000000-JY8M4/"
+datapath="/home/ngs/200206_M04028_0114_000000000-CWB58/"
+outdir="Analysis/bacteria"
 core=8
 
-if [ ! -d "Analysis/micca_its" ]; then
-	mkdir Analysis/micca_its
+if [ ! -d "${outdir}/micca_16S" ]; then
+	mkdir -p ${outdir}/micca_16S
 fi
 
 # Remove N from assembly
-singularity run $currpath/micca.sif micca filter -i $currpath/Analysis/micca_its/WP1_assembled_ITS.fastq -o $currpath/Analysis/micca_its/WP1_assembled_ITS.fasta --maxns 0
+echo " - filtering reads"
+singularity run $currpath/micca.sif micca filter -i $currpath/${outdir}/micca_16S/WP1_assembled_16S.fastq -o $currpath/${outdir}/micca_16S/WP1_assembled_16S.fasta --maxns 0
 
 # count
-grep -c '>' $currpath/Analysis/micca_its/WP1_assembled_ITS.fasta
+echo " - countig reads after filtering"
+grep -c '>' $currpath/${outdir}/micca_16S/WP1_assembled_16S.fasta
 
 echo "DONE!!"
 
