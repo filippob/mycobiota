c51b5c5139b4e5388a6360ead024c8c4e7f081b4
filippob <filippo.biscarini@gmail.com>
2022-02-10 14:24:56 +0100

    fixing paths and environmental variables

--- scripts_Bacteria/7.otu_picking.sh
+++ scripts_Bacteria/7.otu_picking.sh
@@ -1,15 +1,16 @@
 ## setting the enviornmnent
 currpath=$(pwd)
 datapath="/home/ngs/210915_M04028_0141_000000000-JY8M4/"
+outdir="Analysis/bacteria"
 core=8
 
-if [ ! -d "Analysis/micca_its" ]; then
-	mkdir Analysis/micca_its
+if [ ! -d "${outdir}/micca_16S" ]; then
+	mkdir -p ${outdir}/micca_16S
 fi
 
 ## OTU picking
 # pick otu
-singularity run $currpath/micca.sif micca otu -m denovo_unoise -i $currpath/Analysis/micca_its/WP1_assembled_ITS.fasta -o $currpath/Analysis/micca_its/ -t 8 --rmchim
+singularity run $currpath/micca.sif micca otu -m denovo_unoise -i $currpath/${outdir}/micca_16S/WP1_assembled_16S.fasta -o $currpath/${outdir}/micca_16S/ -t 8 --rmchim
 
 echo "DONE!!"
 
